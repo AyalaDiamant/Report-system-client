186d893cdcc92c229e84a1c83307a6d9e757b60d
AyalaDiamant <Ayala66610@gmail.com>
2024-11-06 13:25:54 +0200

    try3

--- src/components/users/admin/employeeManagement.component.tsx
+++ src/components/users/admin/employeeManagement.component.tsx
@@ -207,7 +207,7 @@ const EmployeeManagement: React.FC = () => {
                                         .sort((a, b) => a.name.localeCompare(b.name))
                                         .map((employee) => (
                                             <li key={employee._id} className="list-group-item d-flex justify-content-between align-items-center">
-                                                {employee.name} - {employee.roles && employee.roles.length > 0 ? employee.roles[0].name : 'תפקיד לא ידוע'}
+                                                {employee.name} - {employee.roles && employee.roles.length > 0 ? employee.roles[0].name : ''}
                                                 <div>
                                                     <button type="button" className="btn btn-warning btn-sm" onClick={() => setEditEmployee(employee)}>ערוך</button>
                                                     <button type="button" className="btn btn-danger btn-sm" onClick={() => handleDeleteEmployee(employee._id)}>מחק</button>
